2d05033bdabe355e3c72e28d8e0a93a49570054f
Rosonatt Ferreira <h2oroso@gmail.com>
2024-12-11 10:52:35 -0300

    curso

---
+++ Codigos/codigo_3.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Rosonatt Ferreira')
+print(1,'"Rosonatt Ferreira"')
+
+# Aspas duplas
+print("Rosonatt Ferreira")
+print(2,"'Rosonatt Ferreira'")
+# Escape
+print('Rosonatt \"Ferreira\"')
+
+# r
+print(r'Rosonatt \"Ferreira\"')
---
+++ Codigos/codigo_8_exercicio_1.py
@@ -0,0 +1,13 @@
+nome = 'Rosonatt'
+sobrenome = 'Ferreira'
+idade = 29
+ano_nascimento = 1995
+maior_de_idade = idade >= 18
+altura_metros = float ('1.68')
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
