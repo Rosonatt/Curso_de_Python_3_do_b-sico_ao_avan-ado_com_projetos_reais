8d36ceda7f3294e6f3abeab92dbdf20f7df65cfd
Rosonatt Ferreira <h2oroso@gmail.com>
2024-12-12 12:47:39 -0300

    ATT

---
+++ Codigos/26-30/codigo_28_exercicio_4.py
@@ -0,0 +1,35 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+
+nome = input(f'qual é o seu nome? ')
+idade = input(f'qual é a sua idade? ')
+
+if not nome or not idade:
+    print(f'Desculpe, você deixou campos vazios ')
+else:  
+    print (f'Seu nome é {nome}')
+    print (f'Seu nome invertido é {nome[:: -1]}')
+
+    if " " in nome:
+        print (f'seu nome contém espaços')
+    else:
+        print(f'seu nome não contem espaços') 
+
+    print (f'Seu nome tem',(len(nome)), 'letras')
+
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+
